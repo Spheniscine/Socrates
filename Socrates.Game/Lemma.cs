8039b967b6bf62632dc7b9363cda42b9b447a911
Luke Wolf <lukewolf101010@gmail.com>
2014-04-12 00:24:24 -0700

    Implement Lemma Class

--- Socrates.Game/Lemma.cs
+++ Socrates.Game/Lemma.cs
@@ -13,14 +13,76 @@
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
 using System;
+using System.Diagnostics;
 
 namespace Socrates.Game
 {
-    public class Lemma
+	/// <summary>
+	/// A Lemma is a string of letters from A-Z. It may not contain numbers
+	/// and is case insensitive.  A lemma should be no more than 40 characters long 
+	/// as going longer than that makes rules difficult to discern.
+	/// </summary>
+	public class Lemma
     {
+		#region Constructors
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Socrates.Game.Lemma"/> class.
+		/// </summary>
         public Lemma()
         {
+
         }
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Socrates.Game.Lemma"/> class.
+		/// </summary>
+		/// <param name="content">
+		/// Do not make more than 40 characters long. While we do not check, 
+		/// if you are following this guideline, it makes it difficult for players to discern rules.
+		/// </param>
+		public Lemma(String content)
+		{
+			Content = content.ToLower();
+			Debug.WriteIf(Content.Length > 40, "Lemma Longer than 40 Characters");
+
+		}
+		#endregion
+		#region Methods
+		public override string ToString()
+		{
+			return string.Format("{0}", Content);
+		}
+		#endregion
+		#region Properties
+		/// <summary>
+		/// The String representing the Lemma.
+		/// </summary>
+		/// <value>The String representing the Lemma.</value>
+		public String Content
+		{
+			get;
+			private set; //Contents of a Lemma should be read only
+		}
+		public bool IdealForm
+		{
+			get;
+			set
+			{
+				//Should only be able to set correct to true, it can't become false
+				//TODO: make it so only the master can set a Lemma to correct
+				if(idealForm)//== true
+				{
+					return;
+				}
+				if (value)//== true
+				{
+					idealForm = true;
+				}
+			}
+		}
+		#endregion
+		#region Private Variables
+		private bool idealForm = false;
+		#endregion
     }
 }
 
